a25ddd4bba7ea5a20d16dc34fb0c2f099a247486
Luciano <lucianocassio66@gmail.com>
2023-05-03 19:53:36 -0300

    exercicio2

---
+++ exercicio2/script.js
@@ -0,0 +1,10 @@
+const pergunta1 = 'Você está pelo computador?'
+const pergunta2 = 'Seu olho é claro?'
+const pergunta3 = 'Você mora no Nordeste?'
+const resposta1 = prompt(pergunta1)
+const resposta2 = prompt(pergunta2)
+const resposta3 = prompt(pergunta3)
+
+console.log(pergunta1, '-', resposta1)
+console.log(pergunta2, '-', resposta2)
+console.log(pergunta3, '-', resposta3)
